20ac712b3e4a8d4499ee090b4c4eb3cb059d7dcf
mfurukawa <masa.redmage@gmail.com>
2018-07-03 13:04:57 +0900

    init

---
+++ hello/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "context"
+
+  "github.com/aws/aws-lambda-go/events"
+  "github.com/aws/aws-lambda-go/lambda"
+)
+
+func hello(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+  return events.APIGatewayProxyResponse{Body: "Hello World!", StatusCode: 200}, nil
+}
+
+func main() {
+  lambda.Start(hello)
+}
